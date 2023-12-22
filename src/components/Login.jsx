9c40c8308a1e4529c24a4ee01b5fb59094e6f956
Gaurav-2503 <gauravbhagat958@gmail.com>
2023-12-22 17:24:03 +0530

    4th task completed partially

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -33,67 +33,80 @@ const Login = () => {
     const handleSubmit = (event) => {
       event.preventDefault();
       if (password === "SmartServTest@123") {
-        // Redirect to the dashboard if the credentials are correct
         console.log("Helllo")
         redirect('/dashboard');
       } else {
-        // Handle incorrect credentials (show an error message or any other action)
         console.log("Incorrect credentials");
       }
     };
 
+    const handleForgotPasswordClick = () => {
+      const supportEmail = "support@smartserv.io";
+      const subject = "Password Reset Request";
+      const body = "Please provide details for password reset..."
+
+      const mailtoLink = `mailto:${supportEmail}?subject=${encodeURIComponent(
+        subject
+      )}&body=${encodeURIComponent(body)}`;
+
+      // Open the default email client
+      window.location.href = mailtoLink;
+    };
+
+
+
     return (
       <div className="App flex flex-col  sm:w-full justify-center">
-      <div className="bg-black py-14 text-white rounded-3xl">
-        <div className="logo flex flex-col items-center">
-          <div className="logoImg h-[62px] w-[62px]">
-            <img src={logo} className="h-[62px] w-[62px]"></img>
-          </div>
-          <div className="mb-6">
-            <div className="text-[#71be58] text-[52px] font-bold">
-              SMART
-              <span className="text-white text-[52px] font-bold">
-                S<span className="ServE">E</span>RV
-              </span>
+        <div className="bg-black py-14 text-white rounded-3xl">
+          <div className="logo flex flex-col items-center">
+            <div className="logoImg h-[62px] w-[62px]">
+              <img src={logo} className="h-[62px] w-[62px]"></img>
+            </div>
+            <div className="mb-6">
+              <div className="text-[#71be58] text-[52px] font-bold">
+                SMART
+                <span className="text-white text-[52px] font-bold">
+                  S<span className="ServE">E</span>RV
+                </span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <form onSubmit={handleSubmit}>
-          <div className="ipt">
-            <div className="">
-              <input
-                type="text"
-                placeholder="Username"
-                className={`text-black w-3/4 border my-2 py-1.5 px-4 rounded-sm ${
-                  isValidEmail ? "" : "border-red"
-                }`}
-                value={username}
-                onChange={handleUsernameChange}
-              />
-            </div>
-            {!isValidEmail && (
-              <p className="text-red-500 text-sm">Invalid email format</p>
-            )}
+          <form onSubmit={handleSubmit}>
+            <div className="ipt">
+              <div className="">
+                <input
+                  type="text"
+                  placeholder="Username"
+                  className={`text-black w-3/4 border my-2 py-1.5 px-4 rounded-sm ${
+                    isValidEmail ? "" : "border-red"
+                  }`}
+                  value={username}
+                  onChange={handleUsernameChange}
+                />
+              </div>
+              {!isValidEmail && (
+                <p className="text-red-500 text-sm">Invalid email format</p>
+              )}
 
-            <div className="">
-              <input
-                type="password"
-                placeholder="Password"
-                className={` text-black w-3/4 border my-2 py-1.5 px-4 rounded-sm ${
-                  isValidPassword ? "" : "border-red"
-                }`}
-                value={password}
-                onChange={handlePasswordChange}
-              />
+              <div className="">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className={` text-black w-3/4 border my-2 py-1.5 px-4 rounded-sm ${
+                    isValidPassword ? "" : "border-red"
+                  }`}
+                  value={password}
+                  onChange={handlePasswordChange}
+                />
+              </div>
+              {!isValidPassword && (
+                <p className="text-red-500 text-sm">
+                  Password must contain an uppercase letter, a number, and only
+                  @ as a special character.
+                </p>
+              )}
             </div>
-            {!isValidPassword && (
-              <p className="text-red-500 text-sm">
-                Password must contain an uppercase letter, a number, and only @
-                as a special character.
-              </p>
-            )}
-          </div>
 
             <button
               type="submit"
@@ -104,10 +117,12 @@ const Login = () => {
             </button>
 
             <div className="text-center underline hover:cursor-pointer -mb-2  text-[#7a7a7a]">
-              <span className="">Forget your password?</span>
+              <a onClick={handleForgotPasswordClick} className="">
+                Forget your password?
+              </a>
             </div>
-        </form>
-      </div>
+          </form>
+        </div>
       </div>
     );
 
